66cd9e704eb0cb879b33b468348293fe5f5eb342
Fergal McNamara <g00180082@atu.ie>
2025-02-07 19:45:27 +0000

    **** Prompt for second number ****

--- src/main/java/ie/atuCalc_1_Week3/Calc_1.java
+++ src/main/java/ie/atuCalc_1_Week3/Calc_1.java
@@ -14,5 +14,11 @@ public class Calc_1 {
         System.out.println("Please enter a number: ");
         double firstNum = scan1.nextDouble();
         System.out.println("You entered " + firstNum);
+
+        // Please enter second number
+        System.out.println("Please enter second number: ");
+        double secondNum = scan1.nextDouble();
+        System.out.println("You entered " + secondNum);
+
     }
 }
