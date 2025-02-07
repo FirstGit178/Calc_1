2f5f9798ab9f52d1634a8b2c89ec70581127da66
Fergal McNamara <g00180082@atu.ie>
2025-02-07 19:27:08 +0000

    **** Initial Commit ****

---
+++ src/main/java/ie/atuCalc_1_Week3/Calc_1.java
@@ -0,0 +1,13 @@
+package ie.atuCalc_1_Week3;
+
+
+
+public class Calc_1 {
+
+    public static void main(String[] args) {
+        //Tasks to perform
+        // 1 Prompt the user for a number
+        // 2 Prompt the user for a second number
+        // 3 Display the total to the screen
+    }
+}
